f877cf648095e60268e114fdbb501edc4db52d08
CruzTotvs <edinei.pereira@totvs.com.br>
2024-08-16 07:16:02 -0300

    :construction:FirstStepsGolang

--- main.go
+++ main.go
@@ -2,26 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"reflect"
 )
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Iniciando Monitormaneto...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do Programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não reconheço este comando...")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			startMonitoring()
+			// iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do Programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não reconheço este comando...")
+			os.Exit(-1)
+		}
+
 	}
 
 }
@@ -56,3 +62,34 @@ func leComando() int {
 
 	return comandoLido
 }
+
+func startMonitoring() {
+	fmt.Println("Iniciando Monitoramento...")
+
+	site := "https://www.totvs.com.br"
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 200 {
+		fmt.Println("O site ## ", site, " ## foi carregado com sucesso!")
+	} else {
+		fmt.Println("Ocorreu um erro: Status Code:", resp.StatusCode)
+	}
+}
+
+// func iniciarMonitoramento() {
+// 	fmt.Println("Monitorando...")
+// 	// site com URL inexistente
+// 	site := "https://www.alura.com.br" // ou 200
+// 	resp, _ := http.Get(site)
+
+// 	if resp.StatusCode == 200 {
+// 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+// 	} else {
+// 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+// 	}
+// }
