a791df3a812a8aab443c5b4a427a41b7bf0a3798
CruzTotvs <edinei.pereira@totvs.com.br>
2024-08-25 05:43:43 -0300

    :rocket:First Golang project

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
-	"reflect"
+	"strconv"
+	"strings"
 	"time"
 )
 
 const monitoramentos = 5
 const intervalo = 5
+const logFilePath = "log.txt"
+const sitesFilePath = "sites.txt"
 
 func main() {
 	exibeIntroducao()
@@ -23,8 +28,8 @@ func main() {
 			startMonitoring()
 			// iniciarMonitoramento()
 		case 2:
-			fmt.Println("Exibindo Logs...")
-			//treinaFor()
+			exibeLogs()
+			// Exibindo logs
 		case 0:
 			fmt.Println("Saindo do Programa...")
 			os.Exit(0)
@@ -48,13 +53,15 @@ func exibeIntroducao() {
 
 	fmt.Println(message)
 	fmt.Println(msgPrograma, versao)
-	fmt.Println("A variável versão e do tipo:", reflect.TypeOf(versao))
+	//fmt.Println("A variável versão e do tipo:", reflect.TypeOf(versao))
 }
 
 func exibeMenu() {
+	pulaLina()
 	fmt.Println(" 1 - Iniciar Monitormanento")
 	fmt.Println(" 2 - Exibir Logs")
-	fmt.Println(" O Sair do Programa")
+	fmt.Println(" 0 - Sair do Programa")
+	pulaLina()
 }
 
 func leComando() int {
@@ -70,7 +77,9 @@ func leComando() int {
 
 func startMonitoring() {
 	fmt.Println("Iniciando Monitoramento...")
-	sites := []string{"https://www.alura.com.br", "https://www.totvs.com.br", "https://www.goTTgle.com"}
+
+	sites := readSitesInFile()
+
 	for i := 0; i < monitoramentos; i++ {
 
 		for i, site := range sites {
@@ -78,10 +87,10 @@ func startMonitoring() {
 			TestSites(site)
 		}
 		time.Sleep(time.Second * intervalo)
-		fmt.Println("")
+		pulaLina()
 	}
 
-	fmt.Println("")
+	pulaLina()
 
 }
 
@@ -105,16 +114,76 @@ func startMonitoring() {
 //		}
 //	}
 func TestSites(site string) {
+
 	resp, err := http.Get(site)
+
 	if err != nil {
 		fmt.Println("Ocorreu um erro:", err)
 		return
 	}
-	defer resp.Body.Close()
+
+	//defer resp.Body.Close()
 
 	if resp.StatusCode == 200 {
 		fmt.Println("O site ## ", site, " ## foi carregado com sucesso!")
+		registaLog(site, true)
 	} else {
-		fmt.Println("Ocorreu um erro: Status Code:", resp.StatusCode)
+		fmt.Println("Ocorreu um erro: Status Code:", resp.StatusCode, " do site:", site)
+		registaLog(site, false)
 	}
+}
+
+func readSitesInFile() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open(sitesFilePath)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+
+	return sites
+}
+
+func registaLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666) //0666 = permissoes de escrita e leitura
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n") // 02/01/2006 15:04:05 = formato da data conforme documentação do Go
+
+	arquivo.Close()
+}
+
+func exibeLogs() {
+	fmt.Println("Exibindo Logs...")
+	arquivo, err := os.ReadFile(logFilePath)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	fmt.Println(string(arquivo))
+}
+
+func pulaLina() {
+	fmt.Println("")
 }
